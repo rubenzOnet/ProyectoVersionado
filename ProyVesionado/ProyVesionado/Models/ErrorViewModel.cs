ca44109b6acf4795fafd894cc72cfd0a3be6acd5
Rubenz <irubenzmx@gmail.com>
2020-04-25 16:58:01 -0500

    Version inicial

---
+++ ProyVesionado/ProyVesionado/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyVesionado.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
